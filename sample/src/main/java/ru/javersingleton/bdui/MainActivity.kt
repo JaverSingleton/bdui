11318fcd043df40939b855c98e5e0b248a04bfef
Igor Solkin <inbox@tomclaw.com>
2022-11-22 19:53:37 +0300

    Add components parser

--- sample/src/main/java/ru/javersingleton/bdui/MainActivity.kt
+++ sample/src/main/java/ru/javersingleton/bdui/MainActivity.kt
@@ -1,81 +1,101 @@
 package ru.javersingleton.bdui
 
 import android.os.Bundle
+import android.util.JsonReader
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
+import org.json.JSONArray
+import org.json.JSONObject
 import ru.javersingleton.bdui.component.compose.BeduinComponent
 import ru.javersingleton.bdui.core.BeduinController
 import ru.javersingleton.bdui.core.field.*
 import ru.javersingleton.bdui.core.plus
 
 class MainActivity : AppCompatActivity() {
-    private val beduin: BeduinController = BeduinController(
-        ComponentField(
-            type = "Column",
-            "children" to ArrayField(
-                ComponentField(
-                    type = "Column",
-                    "children" to ArrayField(
-                        ComponentField(
-                            type = "Toolbar",
-                            "title" to PrimitiveField("Contacts"),
-                        ),
-                        ComponentField(
-                            type = "Text",
-                            "text" to PrimitiveField("Contacts"),
-                            "textSize" to PrimitiveField("24"),
-                            "textAlign" to PrimitiveField("Center"),
-                        ),
-                        ComponentField(
-                            type = "ContactItem",
-                            "avatar" to PrimitiveField("https://www.meme-arsenal.com/memes/25bd41b4371cc7b1c206f98a1619b3cb.jpg"),
-                            "name" to PrimitiveField("Happy Cat"),
-                            "lastSeen" to PrimitiveField("21.11.2022"),
-                        ),
-                        ComponentField(
-                            type = "ContactItem",
-                            "avatar" to PrimitiveField("https://cdn-cbeko.nitrocdn.com/YAysSGytvxeVxHWdRPueSoYmAixjAhdB/assets/static/optimized/rev-a51233b/wp-content/uploads/2021/07/broken-15.jpeg"),
-                            "name" to PrimitiveField("Broken Cat"),
-                            "lastSeen" to PrimitiveField("20.11.2022"),
-                            "online" to PrimitiveField("false"),
-                            id = "contact2"
-                        ),
-                        ComponentField(
-                            type = "Box",
-                            "layout_height" to PrimitiveField("fillMax"),
-                            "children" to ArrayField(
-                                ComponentField(
-                                    type = "Button",
-                                    "text" to PrimitiveField("Submit"),
-                                    "layout_alignment" to PrimitiveField("BottomCenter"),
-                                ),
-                            )
-                        )
-                    ),
-                )
-            ),
-        )
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val root = assetComponent("sample1.json")
+        val controller = BeduinController(root)
         setContent {
             Column {
                 BeduinComponent(
-                    controller = beduin,
-                    modifier = Modifier.clickable {
-                        beduin.state += ComponentField(
-                            type = "ContactItem",
-                            "online" to PrimitiveField("true"),
-                            id = "contact2"
-                        )
-                    }.fillMaxWidth()
+                    controller = controller,
+                    modifier = Modifier
+                        .clickable {
+                            controller.state += assetComponent("sample1_patch1.json")
+                        }
+                        .fillMaxWidth()
                 )
             }
         }
     }
+
+    private fun assetComponent(name: String): ComponentField {
+        val json = readAsset(name)
+        val obj = JSONObject(json)
+        return parseComponentField(obj) as ComponentField
+    }
+
+    private fun readAsset(name: String): String {
+        return application.assets.open(name).bufferedReader().run { readText() }
+    }
+
+    private fun parseComponentField(value: Any): Field<*> {
+        val obj = value as JSONObject
+        var type = ""
+        var id: String = newId()
+        val fields = ArrayList<Pair<String, Field<*>>>()
+        obj.keys().forEach { key ->
+            when (key) {
+                "type" -> type = obj.getString(key)
+                "id" -> id = obj.getString(key)
+                "children" -> fields += key to ArrayField(
+                    fields = parseArrayField(
+                        obj.getJSONArray(key),
+                        ::parseComponentField
+                    )
+                )
+                else -> fields += key to parseField(obj.get(key))
+            }
+        }
+        return ComponentField(type, fields = fields.toTypedArray(), id = id)
+    }
+
+    private fun parseField(value: Any): Field<*> {
+        return when (value) {
+            is JSONArray -> ArrayField(fields = parseArrayField(value, ::parseField))
+            is JSONObject -> StructureField(fields = parseStructureField(value))
+            else -> {
+                val strVal = value.toString()
+                val isRef = strVal.startsWith("{{").and(strVal.endsWith("}}"))
+                if (isRef) {
+                    ReferenceField(strVal.substring(2, strVal.length - 2))
+                } else {
+                    PrimitiveField(strVal)
+                }
+            }
+        }
+    }
+
+    private fun parseArrayField(arr: JSONArray, parseFunc: (Any) -> Field<*>): Array<Field<*>> {
+        val fields = ArrayList<Field<*>>()
+        for (i in 0 until arr.length()) {
+            fields += parseFunc(arr[i])
+        }
+        return fields.toTypedArray()
+    }
+
+    private fun parseStructureField(obj: JSONObject): Array<Pair<String, Field<*>>> {
+        val fields = ArrayList<Pair<String, Field<*>>>()
+        obj.keys().forEach { key ->
+            fields += key to parseField(obj.get(key))
+        }
+        return fields.toTypedArray()
+    }
+
 }
